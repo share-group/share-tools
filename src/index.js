8a696d6f7fcddb5a099cb6632ae9481f354b2ea3
june <zruan@kalengo.com>
2017-07-21 13:46:26 +0800

    还是用antd-admin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import dva from 'dva';
-import './index.css';
+import './index.html'
+import 'babel-polyfill'
+import dva from 'dva'
+import createLoading from 'dva-loading'
+import { browserHistory } from 'dva/router'
+import { message } from 'antd'
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  ...createLoading({
+    effects: true,
+  }),
+  history: browserHistory,
+  onError (error) {
+    message.error(error.message)
+  },
+})
 
-// 2. Plugins
-// app.use({});
+// 2. Model
+app.model(require('./models/app'))
 
-// 3. Model
-// app.model(require('./models/example'));
+// 3. Router
+app.router(require('./router'))
 
-// 4. Router
-app.router(require('./router'));
-
-// 5. Start
-app.start('#root');
+// 4. Start
+app.start('#root')
