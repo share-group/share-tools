ab2448efb2750cb7eb8b8ab3d46cd496a1100d88
june <zruan@kalengo.com>
2017-07-21 13:36:05 +0800

    update .gitignore

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
+import Products from './routes/Products';
+
+<Route path="/products" component={Products} />
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
