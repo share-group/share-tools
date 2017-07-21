07d2832c7028641949fd0cabdafc03dfcd455b98
june <zruan@kalengo.com>
2017-07-21 13:13:48 +0800

    用dva试试

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-import './index.html'
-import 'babel-polyfill'
-import dva from 'dva'
-import createLoading from 'dva-loading'
-import { browserHistory } from 'dva/router'
-import { message } from 'antd'
+import dva from 'dva';
+import './index.css';
 
 // 1. Initialize
-const app = dva({
-  ...createLoading(),
-  history: browserHistory,
-  onError (error) {
-    message.error(error.message)
-  },
-})
+const app = dva();
 
-// 2. Model
-app.model(require('./models/app'))
+// 2. Plugins
+// app.use({});
 
-// 3. Router
-app.router(require('./router'))
+// 3. Model
+// app.model(require('./models/example'));
 
-// 4. Start
-app.start('#root')
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
