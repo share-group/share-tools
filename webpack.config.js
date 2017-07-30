63ebd0a862b8af8991d9581c071ff8b3630072f0
june <zruan@kalengo.com>
2017-07-30 16:11:11 +0800

    自己搭

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-eval-source-map',
+  devtool: 'nosources-source-map',
   entry: {
     app: './src/index.js',
     vendor: ['lodash']
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin()
+    new HTMLWebpackPlugin({
+      hash: true,
+      mobile: true,
+      filename: 'index.html',
+      template: 'src/index.html',
+      minify: {
+        collapseWhitespace: true,
+      }
+    })
   ],
+  module: {
+    rules: [{
+      test: /\.less$/,
+      loader: "style-loader!css-loader"
+    }]
+  },
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
@@ -19,6 +35,7 @@ module.exports = {
   devServer: {
     hot: true, // 告诉 dev-server 我们在使用 HMR
     contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    compress: true,
+    port: 9000
   }
 };
