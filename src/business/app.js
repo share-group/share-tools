de7d8a112235e05e429984887a42d4455b4255bc
june <zruan@kalengo.com>
2017-07-31 22:23:26 +0800

    研究Link跳转的方法

--- src/business/app.js
+++ src/business/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router'
-import { Menu, Icon } from 'antd';
+import {Link} from 'react-router';
+import {Menu, Icon} from 'antd';
 
 const SubMenu = Menu.SubMenu;
 
@@ -20,23 +20,23 @@ module.exports = class extends React.Component {
     });
   }
 
-  render() {
+  render () {
     return (
       <div className="side">
         <Menu
           theme={this.state.theme}
           onClick={this.handleClick}
-          style={{ width: 240 }}
+          style={{width: 240}}
           selectedKeys={[this.state.current]}
           mode="inline"
         >
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>字符串工具</span></span>}>
-            <Link to="https://www.baidu.com"><Menu.Item>MD5</Menu.Item></Link>
+          <SubMenu key="sub1" title={<span><Icon type="mail"/><span>字符串工具</span></span>}>
+            <Menu.Item key="1"><a href="/xxxx">xxxxxxxxxx</a></Menu.Item>
             <Menu.Item key="2">base64</Menu.Item>
             <Menu.Item key="3">Option 3</Menu.Item>
             <Menu.Item key="4">Option 4</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigtion Two</span></span>}>
+          <SubMenu key="sub2" title={<span><Icon type="appstore"/><span>Navigtion Two</span></span>}>
             <Menu.Item key="5">Option 5</Menu.Item>
             <Menu.Item key="6">Option 6</Menu.Item>
             <SubMenu key="sub3" title="Submenu">
@@ -44,7 +44,7 @@ module.exports = class extends React.Component {
               <Menu.Item key="8">Option 8</Menu.Item>
             </SubMenu>
           </SubMenu>
-          <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+          <SubMenu key="sub4" title={<span><Icon type="setting"/><span>Navigation Three</span></span>}>
             <Menu.Item key="9">Option 9</Menu.Item>
             <Menu.Item key="10">Option 10</Menu.Item>
             <Menu.Item key="11">Option 11</Menu.Item>
